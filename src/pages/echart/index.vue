3966d17165350b20209af4064143b2a5537be686
unknown <wangpeng04@ppdai.com>
2019-11-22 17:17:28 +0800

    add

--- src/pages/echart/index.vue
+++ src/pages/echart/index.vue
@@ -1,13 +1,46 @@
 <template>
 	<div class="echart">
-
+		<div id="main" style="height:400px"></div>
 	</div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 export default {
+	mounted() {
+		// 基于准备好的dom，初始化echarts图表
+		const  myChart = echarts.init(document.getElementById('main'))
 
+		const  option = {
+			tooltip: {
+				show: true
+			},
+			legend: {
+				data: ['销量']
+			},
+			xAxis: [
+				{
+					type: 'category',
+					data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+				}
+			],
+			yAxis: [
+				{
+					type: 'value'
+				}
+			],
+			series: [
+				{
+					name: '销量',
+					type: 'bar',
+					data: [5, 20, 40, 10, 10, 20]
+				}
+			]
+		}
+
+		// 为echarts对象加载数据
+		myChart.setOption(option)
+	}
 }
 </script>
 
